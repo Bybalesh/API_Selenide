1a55358efe8112bcc904dc3ca9945754fa3779a7
Nikita Kruglov <bybalesh234@gmail.com>
2023-06-26 19:50:03 +0600

    Finish commit

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -1,9 +1,14 @@
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 
 public class CardTest {
     @Test
-    void cardTest (){
+    void cardTest () throws InterruptedException {
       open("http://localhost:9999");
 
       SelenideElement form = $("form");
@@ -11,6 +16,7 @@ public class CardTest {
       form.$("[data-test-id=phone] input").sendKeys("+79135589627");
       form.$("[data-test-id=agreement]").click();
       form.$("button").click();
-      $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена.С вами свяжется наш менеджер в ближайшее время!"));
+      $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+      Thread.sleep(5000);
     }
 }
